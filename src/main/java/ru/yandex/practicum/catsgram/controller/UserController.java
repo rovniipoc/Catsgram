64252f3ce23b80efe80c2e94cec51d8f9f817d79
Estet <7vt-blest@rambler.ru>
2024-09-15 16:35:59 +0400

    Разделить на service и controller, добавить проверку по id автора поста

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,77 +1,35 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final Map<Long, User> users = new HashMap<>();
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Email должен быть указан");
-        }
-        if (users.values()
-                .stream()
-                .map(User::getEmail)
-                .anyMatch(email -> email.equals(user.getEmail()))) {
-            throw new DuplicatedDataException("Этот email уже используется");
-        }
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-        users.put(user.getId(), user);
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User user) {
-        if (user.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-        if (users.containsKey(user.getId())) {
-            User oldUser = users.get(user.getId());
-            if (users.values()
-                    .stream()
-                    .map(User::getEmail)
-                    .anyMatch(email -> email.equals(user.getEmail()))) {
-                throw new DuplicatedDataException("Этот email уже используется");
-            }
-            if (user.getEmail() != null) {
-                oldUser.setEmail(user.getEmail());
-            }
-            if (user.getUsername() != null) {
-                oldUser.setUsername(user.getUsername());
-            }
-            if (user.getPassword() != null) {
-                oldUser.setPassword(user.getPassword());
-            }
-            return oldUser;
-        }
-        throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден");
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.update(user);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -0,0 +1,78 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    public User create(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Email должен быть указан");
+        }
+        if (users.values()
+                .stream()
+                .map(User::getEmail)
+                .anyMatch(email -> email.equals(user.getEmail()))) {
+            throw new DuplicatedDataException("Этот email уже используется");
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    public User update(User user) {
+        if (user.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(user.getId())) {
+            User oldUser = users.get(user.getId());
+            if (users.values()
+                    .stream()
+                    .map(User::getEmail)
+                    .anyMatch(email -> email.equals(user.getEmail()))) {
+                throw new DuplicatedDataException("Этот email уже используется");
+            }
+            if (user.getEmail() != null) {
+                oldUser.setEmail(user.getEmail());
+            }
+            if (user.getUsername() != null) {
+                oldUser.setUsername(user.getUsername());
+            }
+            if (user.getPassword() != null) {
+                oldUser.setPassword(user.getPassword());
+            }
+            return oldUser;
+        }
+        throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден");
+    }
+
+    public Optional<User> findUserById(Long id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
